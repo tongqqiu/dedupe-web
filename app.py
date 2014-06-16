6ecec26e9c4dad07042bf652150e55c8b7652b72
Eric van Zanten <eric.vanzanten@gmail.com>
2014-06-16 19:34:51 +0000

    Reverted Dedupe API Changes

--- app.py
+++ app.py
@@ -182,7 +182,8 @@ def get_pair():
         deduper = dedupers[deduper_id]['deduper']
         filename = flask_session['filename']
         dedupers[deduper_id]['last_interaction'] = datetime.now()
-        fields = [f[0] for f in deduper.data_model.field_comparators]
+        #fields = [f[0] for f in deduper.data_model.field_comparators]
+        fields = deduper.data_model.field_comparators
         record_pair = deduper.uncertainPairs()[0]
         dedupers[deduper_id]['current_pair'] = record_pair
         data = []
